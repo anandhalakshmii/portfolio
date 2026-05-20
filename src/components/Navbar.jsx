a7e56f49a5f5d9bd2352619e01de303b49004f39
anandhalakshmii <anandhalakshmip16@gmail.com>
2026-05-20 18:59:03 +0530

    MinorUpdates

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,10 +6,20 @@ import {
   Box,
   IconButton
 } from "@mui/material";
+import CloudDownloadIcon from "@mui/icons-material/CloudDownload";
 import GitHubIcon from "@mui/icons-material/GitHub";
 import LinkedInIcon from "@mui/icons-material/LinkedIn";
 
 export default function Navbar() {
+  const handleDownloadResume = () => {
+    const link = document.createElement("a");
+    link.href = "/anandhalakshmi_resume.pdf"; 
+    link.download = "anandhalakshmi_resume.pdf";
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
+  };
+
   return (
     <AppBar
       position="fixed"
@@ -49,11 +59,17 @@ export default function Navbar() {
           <Button color="primary" href="#contact">
             Contact
           </Button>
+          <Button color="primary">
+            Resume
+            <CloudDownloadIcon
+              sx={{ ml: 0.5 }}
+              onClick={handleDownloadResume}
+            />
+          </Button>
 
           {/* Divider Space */}
           <Box sx={{ width: 20 }} />
 
-          {/* GitHub Icon */}
           <IconButton
             href="https://github.com/anandhalakshmii/"
             target="_blank"
